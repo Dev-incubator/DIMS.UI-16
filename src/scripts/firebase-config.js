c42686fd12f6876db303c385d280263eb8f812c1
oleg3555 <olegyanusik4@gmail.com>
2022-03-10 01:55:11 +0300

    refactor(env): correct env file

--- src/scripts/firebase-config.js
+++ src/scripts/firebase-config.js
@@ -11,6 +11,8 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
+console.log(firebaseConfig);
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
